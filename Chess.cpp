de2fe08fd7889cd7a6f2fade962227cff37a751d
CrazzyBeer <crazzybeer@yahoo.com>
2016-03-05 15:11:30 +0200

    .

--- Chess.cpp
+++ Chess.cpp
@@ -7,7 +7,7 @@ bool color(int x, int y) {
 }
 
 bool rook(int x1, int y1, int x2, int y2) {
-    return (x1==x2 || y1 == y2);
+    return (x1 == x2 || y1 == y2);
 }
 
 bool bishop(int x1, int y1, int x2, int y2) {
@@ -15,7 +15,7 @@ bool bishop(int x1, int y1, int x2, int y2) {
 }
 
 bool knight(int x1, int y1, int x2, int y2) {
-    return (abs(x1-x2)*abs(y1-y2) == 2);
+    return (abs(x1-x2) * abs(y1-y2) == 2);
 }
 
 bool king(int x1, int y1, int x2, int y2) {
@@ -24,16 +24,16 @@ bool king(int x1, int y1, int x2, int y2) {
 
 bool pawn(int x1, int y1, int x2, int y2) {
     return (x1 == x2 && (
-                 (y1 != 8 && y1>y2 &&
+                 (y1 != 8 && y1 > y2 &&
                     (
-                     (y1 == 7 && y1-y2<3) ||
-                     (y1-y2<2)
+                     (y1 == 7 && y1 - y2 < 3) ||
+                     (y1 - y2 < 2)
                     )
                   ) ||
-                 (y1 != 1 && y2>y1 &&
+                 (y1 != 1 && y2 > y1 &&
                     (
-                     (y1 == 2 && y2-y1<3) ||
-                     (y2-y1<2)
+                     (y1 == 2 && y2 - y1 < 3) ||
+                     (y2 - y1 < 2)
                     )
                  )
              )
@@ -41,7 +41,7 @@ bool pawn(int x1, int y1, int x2, int y2) {
 }
 
 bool queen(int x1, int y1, int x2, int y2) {
-    return (x1==x2 || y1 == y2) || (abs(x1-x2) == abs(y1-y2));
+    return (x1 == x2 || y1 == y2) || (abs(x1-x2) == abs(y1-y2));
 }
 
 
