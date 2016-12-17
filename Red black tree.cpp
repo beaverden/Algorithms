5f003adcce6f7e2a0060f5e8f0215fe1f6c6a1b4
Denis Rozimovschii <rozimovschii.denis@yahoo.com>
2016-12-17 22:19:35 +0200

    Updated Red Black Search Tree

--- Red black tree.cpp
+++ Red black tree.cpp
@@ -1,80 +1,148 @@
-struct rb_tree {
-    struct node {
-        int val;
-        node *left = nullptr, *right = nullptr;
-        bool red = true;
-        int sz = 0;
-    };
-    node *root = nullptr;
-    int t;
+template <class T>
+class RedBlackTree
+{
+public:
+    void add(T x)
+    {
+        root = add(this->root, x);
+    }
 
-    bool isRed(node *h) {
+    bool find(T x)
+    {
+        return find(this->root, x);
+    }
+
+    int lower_bound(T x)
+    {
+        return lower_bound(this->root, x);
+    }
+
+    int upper_bound(T x)
+    {
+        return size(root) - lower_bound(x);
+    }
+
+private:
+
+    struct node
+    {
+        T value;
+        node *left = nullptr;
+        node *right = nullptr;
+        bool red = true;
+        int subtree_size = 0;
+    };
+
+    node *root = nullptr;
+
+    bool isRed(node *h)
+    {
         if ( h == nullptr ) return false;
         else return (h->red);
     }
 
-    void add(int x) {
-        root = add(root, x);
+    void flipColors(node *h)
+    {
+        h->red = !h->red;
+        h->left->red = !h->left->red;
+        h->right->red = !h->right->red;
     }
-    node* add(node *h, int x) {
-        if (h == nullptr) {
+
+    node* rotateRight(node *h)
+    {
+        node *x = h->left;
+        h->left = x->right;
+        x->right = h;
+        x->red = x->right->red;
+        x->right->red = true;
+        x->subtree_size = size(h);
+        h->subtree_size = size(h->left) + size(h->right) + 1;
+        return x;
+    }
+
+    node* rotateLeft(node *h)
+    {
+        node *x = h->right;
+        h->right = x->left;
+        x->left = h;
+        x->red = x->left->red;
+        x->left->red = true;
+        x->subtree_size = size(h);
+        h->subtree_size = size(h->left) + size(h->right) + 1;
+
+        return x;
+    }
+
+    int size(node *h)
+    {
+        if ( h == nullptr) return 0;
+        return h->subtree_size;
+    }
+
+    node* add(node *h, T x)
+    {
+        if (h == nullptr)
+        {
             h = new node;
 
-            h->val = x;
-            h->sz = 1;
+            h->value = x;
+            h->subtree_size = 1;
             h->red = true;
 
             return h;
         }
-        if (x < h->val) {
+        if (x < h->value)
+        {
             h->left = add(h->left, x);
-        } else {
+        }
+        else
+        {
             h->right = add(h->right, x);
         }
         if (isRed(h->right) && !isRed(h->left))  h = rotateLeft(h);
         if (isRed(h->left)  &&  isRed(h->left->left)) h = rotateRight(h);
         if (isRed(h->left)  &&  isRed(h->right))     flipColors(h);
 
-        h->sz = size(h->left) + size(h->right) + 1;
+        h->subtree_size = size(h->left) + size(h->right) + 1;
 
         return h;
     }
-    int size(node *h) {
-        if ( h == nullptr) return 0;
-        return h->sz;
-    }
-    void flipColors(node *h) {
-        h->red = !h->red;
-        h->left->red = !h->left->red;
-        h->right->red = !h->right->red;
-    }
-    node* rotateRight(node *h) {
-        node *x = h->left;
-        h->left = x->right;
-        x->right = h;
-        x->red = x->right->red;
-        x->right->red = true;
-        x->sz = size(h);
-        h->sz = size(h->left) + size(h->right) + 1;
-        return x;
-    }
-    node* rotateLeft(node *h) {
-        node *x = h->right;
-        h->right = x->left;
-        x->left = h;
-        x->red = x->left->red;
-        x->left->red = true;
-        x->sz = size(h);
-        h->sz = size(h->left) + size(h->right) + 1;
 
-        return x;
+    bool find(node *h, T x)
+    {
+        if (h == nullptr)
+        {
+            return false;
+        }
+        if (x == h->value)
+        {
+            return true;
+        }
+        else if (x < h->value)
+        {
+            return find(h->left, x);
+        }
+        else
+        {
+            return find(h->right, x);
+        }
     }
-    int lower_bound(node *h, int x) {
-        if (h == nullptr) return 0;
-        int g = h->val;
-        int s = size(h->left);
-        if (x >= h->val) return 1 + size(h->left) + lower_bound(h->right,x);
-        else return lower_bound(h->left,x);
 
+
+    int lower_bound(node *h, int x)
+    {
+        if (h == nullptr)
+        {
+            return 0;
+        }
+        if (x >= h->value)
+        {
+            return 1 + size(h->left) + lower_bound(h->right, x);
+        }
+        else
+        {
+            return lower_bound(h->left, x);
+        }
     }
-} tree;
+
+};
