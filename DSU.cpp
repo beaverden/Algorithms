feb65d41743dcc8c1d605d219a0d64e955a85c91
Denis Rozimovschii <rozimovschii.denis@yahoo.com>
2016-12-19 12:05:12 +0200

    Updated DSU

--- DSU.cpp
+++ DSU.cpp
@@ -1,18 +1,67 @@
-struct dsu {
-    int p[1000],rank[1000];
-    void init() {
-        for (int i=0;i<1000;i++) {
-            p[i]=i;
-            rank[i]=1;
-        }
-    }
-    int parent(int v) {
-        return (p[v]==v)? v : (p[v]=parent(p[v]));
-    }
-    void merge(int x, int y) {
-        if ((x=parent(x))==(y=parent(y))) return;
-        if (rank[y]>rank[x]) swap(x,y);
-        rank[x]+=rank[y];
-        p[y]=x;
-    }
+class DSU
+{
+public:
+	DSU(const int MAX_NODES)
+	{
+		this->components = MAX_NODES;
+		this->parent = new int[MAX_NODES];
+		this->rank = new int[MAX_NODES];
+		for (int i = 0; i < MAX_NODES; i++)
+		{
+			this->parent[i] = i;
+			this->rank[i] = 1;
+		}
+	}
+	~DSU()
+	{
+		delete[] this->parent;
+		delete[] this->rank;
+	}
+
+	int findParent(int u)
+	{
+		if (this->parent[u] == u)
+		{
+			return u;
+		}
+		else
+		{
+			parent[u] = findParent(parent[u]);
+			return parent[u];
+		}
+	}
+
+	void merge(int u, int v)
+	{
+		int uParent = findParent(u);
+		int vParent = findParent(v);
+		if (uParent == vParent) return;
+		
+		if (rank[uParent] > rank[vParent])
+		{
+			rank[uParent] += rank[vParent];
+			parent[vParent] = uParent;
+		}
+		else
+		{
+			rank[vParent] += rank[uParent];
+			parent[uParent] = vParent;
+		}
+		this->components--;
+	}
+
+	bool areConnected(int u, int v)
+	{
+		return findParent(u) == findParent(v);
+	}
+
+	int size(int u)
+	{
+		return rank[findParent(u)];
+	}
+
+private:
+	int * parent;
+	int * rank;
+	int components;
 };
