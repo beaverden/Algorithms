80f3303ae95c503c70e454b55dfc12954780b50b
Denis Rozimovschii <rozimovschii.denis@yahoo.com>
2017-01-19 22:33:40 +0200

    Added quicksort

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,19 +1,34 @@
-void qsort(int left, int right, int arr[]) {
-    if (left >= right) return;
-    int mid = (left + right)/2,
-        l = left, r = right;
-    do {
-        while (arr[l]<arr[mid]) l++;
-        while (arr[r]>arr[mid]) r--;
-
-        if (l<=r) {
-            int k = arr[l];
-            arr[l] = arr[r];
-            arr[r] = k;
-            l++;
-            r--;
+template <class T>
+int partition(T array[], int left, int right, int pivot)
+{
+    while (left <= right)
+    {
+        while (array[left]  < pivot && left <= right) left++;
+        while (array[right] > pivot && left <= right) right--;
+        if (left <= right)
+        {
+            T temp = array[right];
+            array[right] = array[left];
+            array[left] = temp;
+            left++;
+            right--;
         }
-    } while (l<r);
-    if (left<r) qsort(left, r, arr);
-    if (l<right) qsort(l, right, arr);
+    }
+    return left;
+}
+
+///Class T mush have total order and the operators >, <, >=, <= defined
+template <class T>
+void quicksort(T array[], int left, int right)
+{
+    // srand(time(0));
+    if (left >= right) return;
+
+    int pivotIndex = left + rand() % (right-left+1);
+    int pivot = array[pivotIndex];
+
+    int partitionIndex = partition(array, left, right, pivot);
+
+    quicksort(array, left, partitionIndex-1);
+    quicksort(array, partitionIndex, right);
 }
